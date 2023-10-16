ad6b73b762254120e128996cb96b290d5a5037cb
JGMelon22 <jgvasconcellos22@gmail.com>
2023-10-16 11:13:31 -0300

    feat: developing initial page design

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Films</h1>
+      <h1 id="title" >Films</h1>
+      <p id="film-poster" float="left">
+        <img src="https://lh5.googleusercontent.com/Pd-YAUizQ_zKjSFr98K61pI4SbZFCbJ0zBJyy2RLcv1gDhdjQn-s1C7Xu4JbWWX21PB_CiJGwlc_XVCp4k3R4gA7RLgzbM2Mhjxd9XgtaQbfvgIbHYO3-IYimwl5UNCmWJebNpcm" width="200" height="400" /> <span>&nbsp;</span>
+        <img src="https://m.media-amazon.com/images/I/713BWZgZgtL._AC_UF894,1000_QL80_.jpg" width="200" height="400" />  <span>&nbsp;</span>
+        <img src="https://m.media-amazon.com/images/I/51Suhr1p+VL._AC_UF894,1000_QL80_.jpg" width="200" height="400" /> <span>&nbsp;</span>
+        <img src="https://m.media-amazon.com/images/I/81UOBSDQh0L._AC_UF894,1000_QL80_.jpg" width="200" height="400" />
+      </p>
     </>
   )
 }
